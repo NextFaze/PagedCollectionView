534dd81f7e1dc429385cba7a0ad3afd78a09dad6
DrozNF <mdrozdowski@nextfaze.com>
2019-10-31 14:40:13 +1030

    Test compatibility with Swift Package manager

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "PagedCollectionView",
     platforms: [
         .iOS(.v11)
-    ]
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
